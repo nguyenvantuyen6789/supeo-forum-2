7bb7c4263207504dd24e8a6e373518b3f9a42b32
Admin <nguyenvantuyen6789@gmail.com>
2022-02-27 10:39:12 +0700

    01 - Tạo Dự Án Mới

---
+++ src/main/java/com/tuyennguyen/SupeoForum2Application.java
@@ -0,0 +1,13 @@
+package com.tuyennguyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupeoForum2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupeoForum2Application.class, args);
+    }
+
+}
