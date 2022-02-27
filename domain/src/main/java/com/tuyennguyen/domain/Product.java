d3f79cafa75ea42928b46b3938346c89908d8594
Admin <nguyenvantuyen6789@gmail.com>
2022-02-27 11:32:09 +0700

    03 - Tạo Module Domain

---
+++ domain/src/main/java/com/tuyennguyen/domain/Product.java
@@ -0,0 +1,15 @@
+package com.tuyennguyen.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    private Integer productId;
+    private String productName;
+
+}
--- web/src/main/java/com/tuyennguyen/web/WebApplication.java
+++ web/src/main/java/com/tuyennguyen/web/WebApplication.java
@@ -1,13 +1,24 @@
 package com.tuyennguyen.web;
 
+import com.tuyennguyen.domain.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class WebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
+    }
+
+    @GetMapping("s")
+    public String show() {
+        Product product = new Product(1, "abc");
+        System.out.println(product);
+        return "Data 33";
     }
 
 }
